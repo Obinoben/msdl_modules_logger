2359ff8ca809ff4934ff55923b1519357f0bdcd0
Ben Souverbie <ben@quadrumane.com>
2026-04-28 17:33:00 -0400

    log to file

--- msdl_logger/logger.py
+++ msdl_logger/logger.py
@@ -3,7 +3,7 @@
 
 import logging
 
-def get_logger(name="mylogger", debug=False):
+def get_logger(name="mylogger", debug=False, path=None):
     """
     Retourne un logger Python classique pour console.
     """
@@ -15,15 +15,21 @@ def get_logger(name="mylogger", debug=False):
 
     # Evite de créer plusieurs handlers si le logger est déjà configuré
     if not logger.hasHandlers():
+        # Gestionnaire console
         ch = logging.StreamHandler()
         ch.setLevel(level)
-
-        # Format classique : date, niveau, message
         formatter = logging.Formatter(
             fmt="%(asctime)s [%(levelname)s] %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S"
         )
         ch.setFormatter(formatter)
         logger.addHandler(ch)
+
+        # Gestionnaire fichier (si un chemin est fourni)
+        if path:
+            fh = logging.FileHandler(path, mode='a', encoding='utf-8')
+            fh.setLevel(level)
+            fh.setFormatter(formatter)
+            logger.addHandler(fh)
 
     return logger
